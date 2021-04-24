346500e9a4ae29206c0f002e5ce5514d887948d3
didimango <53489397+didimango@users.noreply.github.com>
2021-04-24 23:43:04 +0300

    added public folder

---
+++ public/wookie-movies/movies-movies-module.js
@@ -0,0 +1,54 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["movies-movies-module"],{
+
+/***/ "EA54":
+/*!*****************************************!*\
+  !*** ./src/app/movies/movies.module.ts ***!
+  \*****************************************/
+/*! exports provided: MoviesModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "MoviesModule", function() { return MoviesModule; });
+/* harmony import */ var _angular_common__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/common */ "ofXK");
+/* harmony import */ var _movies_container_movies_container_component__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./movies-container/movies-container.component */ "Cw+K");
+/* harmony import */ var _movies_routing_module__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./movies-routing.module */ "fLFp");
+/* harmony import */ var _movie_movie_component__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./movie/movie.component */ "8ilY");
+/* harmony import */ var _components_movie_slider_movie_slider_component__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./components/movie-slider/movie-slider.component */ "3ZAS");
+/* harmony import */ var swiper_angular__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! swiper/angular */ "UMfn");
+/* harmony import */ var _material_material_module__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ../material/material.module */ "hctd");
+/* harmony import */ var _components_no_results_no_results_component__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ./components/no-results/no-results.component */ "3wmA");
+/* harmony import */ var _components_genre_page_genre_page_component__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! ./components/genre-page/genre-page.component */ "qjKl");
+/* harmony import */ var _components_search_results_search_results_component__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__(/*! ./components/search-results/search-results.component */ "729U");
+/* harmony import */ var _components_star_rating_star_rating_component__WEBPACK_IMPORTED_MODULE_10__ = __webpack_require__(/*! ./components/star-rating/star-rating.component */ "FIs3");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_11__ = __webpack_require__(/*! @angular/core */ "fXoL");
+
+
+
+
+
+
+
+
+
+
+
+
+class MoviesModule {
+}
+MoviesModule.ɵfac = function MoviesModule_Factory(t) { return new (t || MoviesModule)(); };
+MoviesModule.ɵmod = _angular_core__WEBPACK_IMPORTED_MODULE_11__["ɵɵdefineNgModule"]({ type: MoviesModule });
+MoviesModule.ɵinj = _angular_core__WEBPACK_IMPORTED_MODULE_11__["ɵɵdefineInjector"]({ imports: [[_angular_common__WEBPACK_IMPORTED_MODULE_0__["CommonModule"], _movies_routing_module__WEBPACK_IMPORTED_MODULE_2__["MoviesRoutingModule"], swiper_angular__WEBPACK_IMPORTED_MODULE_5__["SwiperModule"], _material_material_module__WEBPACK_IMPORTED_MODULE_6__["MaterialModule"]]] });
+(function () { (typeof ngJitMode === "undefined" || ngJitMode) && _angular_core__WEBPACK_IMPORTED_MODULE_11__["ɵɵsetNgModuleScope"](MoviesModule, { declarations: [_movies_container_movies_container_component__WEBPACK_IMPORTED_MODULE_1__["MoviesContainerComponent"],
+        _movie_movie_component__WEBPACK_IMPORTED_MODULE_3__["MovieComponent"],
+        _components_movie_slider_movie_slider_component__WEBPACK_IMPORTED_MODULE_4__["MovieSliderComponent"],
+        _components_no_results_no_results_component__WEBPACK_IMPORTED_MODULE_7__["NoResultsComponent"],
+        _components_genre_page_genre_page_component__WEBPACK_IMPORTED_MODULE_8__["GenrePageComponent"],
+        _components_search_results_search_results_component__WEBPACK_IMPORTED_MODULE_9__["SearchResultsComponent"],
+        _components_star_rating_star_rating_component__WEBPACK_IMPORTED_MODULE_10__["StarRatingComponent"]], imports: [_angular_common__WEBPACK_IMPORTED_MODULE_0__["CommonModule"], _movies_routing_module__WEBPACK_IMPORTED_MODULE_2__["MoviesRoutingModule"], swiper_angular__WEBPACK_IMPORTED_MODULE_5__["SwiperModule"], _material_material_module__WEBPACK_IMPORTED_MODULE_6__["MaterialModule"]], exports: [_movies_container_movies_container_component__WEBPACK_IMPORTED_MODULE_1__["MoviesContainerComponent"]] }); })();
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=movies-movies-module.js.map
